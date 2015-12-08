6c931d5f21ac3e50aef823ba0424a4dd9299f809
Shohanul Alam <shohan12224943@gmail.com>
2015-12-08 21:35:47 +0600

    added functionalities of image

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,26 +8,123 @@ Route::post('uploadimage', 'ApplyController@upload');
 
 Route::get("options", "ApplyController@options");
 
-Route::get('gray', function(){
+Route::get('showblur', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->blur(100);
+  return $img->response();
+});
+
+Route::get('saveblur', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->blur(100);
+  $img->save((public_path('uploads/theimage.jpg')));
+  return Redirect::to("options");
+});
+
+Route::get('showbright', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->brightness(25);
+  return $img->response();
+});
+
+Route::get('savebright', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->blur(100);
+  $img->save((public_path('uploads/theimage.jpg')));
+  return Redirect::to("options");
+});
+
+Route::get('showdark', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->brightness(-50);
+  return $img->response();
+});
+
+Route::get('savedark', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->dark(-50);
+  $img->save((public_path('uploads/theimage.jpg')));
+  return Redirect::to("options");
+});
+
+Route::get('showinvert', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->invert();
+  return $img->response();
+});
+
+Route::get('saveinvert', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->invert();
+  $img->save((public_path('uploads/theimage.jpg')));
+  return Redirect::to("options");
+});
+
+Route::get('showgamma', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->gamma(.3);
+  return $img->response();
+});
+
+Route::get('savegamma', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->gamma(.3);
+  $img->save((public_path('uploads/theimage.jpg')));
+  return Redirect::to("options");
+});
+
+Route::get('showsharp', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->sharpen(100);
+  return $img->response();
+});
+
+Route::get('savesharp', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->sharpen(100);
+  $img->save((public_path('uploads/theimage.jpg')));
+  return Redirect::to("options");
+});
+
+Route::get('showrotate', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->rotate(-45);
+  return $img->response();
+});
+
+Route::get('saverotate', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->rotate(-45);
+  $img->save((public_path('uploads/theimage.jpg')));
+  return Redirect::to("options");
+});
+
+Route::get('showgray', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->greyscale();
+  return $img->response();
+});
+
+Route::get('savegray', function(){
   $img = Image::make(public_path('uploads/theimage.jpg'))->greyscale();
   $img->save((public_path('uploads/theimage.jpg')));
   return Redirect::to("options");
 });
 
-Route::get('mirror', function(){
+Route::get('showmirror', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->flip("h");
+  return $img->response();
+});
+
+Route::get('savemirror', function(){
   $img = Image::make(public_path('uploads/theimage.jpg'))->flip("h");
    $img->save((public_path('uploads/theimage.jpg')));
   return Redirect::to("options");
 });
 
-Route::get('blue', function(){
-  $img = Image::make(public_path('uploads/theimage.jpg'))->colorize(-100, 0, 100);
+Route::get('showred', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->colorize(100, 0, 0);
+  return $img->response();
+});
+
+Route::get('savered', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->colorize(100, 0, 0);
    $img->save((public_path('uploads/theimage.jpg')));
   return Redirect::to("options");
 });
 
-Route::get('combo', function(){
-  $img = Image::make(public_path('uploads/theimage.jpg'))->greyscale()->flip("h")->colorize(-100, 0, 100);
+Route::get('showblue', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->colorize(0, 0, 100);
+  return $img->response();
+});
+
+Route::get('saveblue', function(){
+  $img = Image::make(public_path('uploads/theimage.jpg'))->colorize(0, 0, 100);
    $img->save((public_path('uploads/theimage.jpg')));
   return Redirect::to("options");
 });
--- resources/views/pages/options.blade.php
+++ resources/views/pages/options.blade.php
@@ -35,43 +35,140 @@
             <ul class="sidebar-nav">
                 <li class="sidebar-brand">
                     <a href="#">
-                        Options Menu
+                        Effects Menu
                     </a>
                 </li>
 
                 <li>
                     <div id="MainMenu">
                       <div class="list-group panel">
-                        <a href="#demo4" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Item 4</a>
+                        <a href="#demo4" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Blur Effect</a>
                         <div class="collapse" id="demo4">
-                          <a href="" class="list-group-item">Subitem 1</a>
-                          <a href="" class="list-group-item">Subitem 2</a>
-                          <a href="" class="list-group-item">Subitem 3</a>
+                          <a href="showblur" class="list-group-item">Show Me</a>
+                          <a href="saveblur" class="list-group-item">Save with Blur</a>
                         </div>
                       </div>
                     </div>
                 </li>
 
                 <li>
-                    <a href="gray">Gray</a>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo3" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Brightness Effect</a>
+                        <div class="collapse" id="demo3">
+                          <a href="showbright" class="list-group-item">Show Me</a>
+                          <a href="savebright" class="list-group-item">Save with Brightness</a>
+                        </div>
+                      </div>
+                    </div>
                 </li>
+
                 <li>
-                    <a href="blue">Blue</a>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo2" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Darkness Effect</a>
+                        <div class="collapse" id="demo2">
+                          <a href="showdark" class="list-group-item">Show Me</a>
+                          <a href="savedark" class="list-group-item">Save with Darkness</a>
+                        </div>
+                      </div>
+                    </div>
                 </li>
+
                 <li>
-                    <a href="mirror">Mirror</a>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo1" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Invert Effect</a>
+                        <div class="collapse" id="demo1">
+                          <a href="showinvert" class="list-group-item">Show Me</a>
+                          <a href="saveinvert" class="list-group-item">Save with Invert</a>
+                        </div>
+                      </div>
+                    </div>
                 </li>
+
                 <li>
-                    <a href="combo">Combo</a>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo0" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Low Gamma Effect</a>
+                        <div class="collapse" id="demo0">
+                          <a href="showgamma" class="list-group-item">Show Me</a>
+                          <a href="savegamma" class="list-group-item">Save with Low Gamma</a>
+                        </div>
+                      </div>
+                    </div>
                 </li>
+
                 <li>
-                    <a href="download">Download</a>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo5" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Sharpness Effect</a>
+                        <div class="collapse" id="demo5">
+                          <a href="showsharp" class="list-group-item">Show Me</a>
+                          <a href="savesharp" class="list-group-item">Save with Sharpness</a>
+                        </div>
+                      </div>
+                    </div>
                 </li>
+
                 <li>
-                    <a href="blur">Practice</a>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo7" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Greyscale Effect</a>
+                        <div class="collapse" id="demo7">
+                          <a href="showgray" class="list-group-item">Show Me</a>
+                          <a href="savegray" class="list-group-item">Save with Greyscale</a>
+                        </div>
+                      </div>
+                    </div>
                 </li>
+
                 <li>
-                    <a href="reset">reset</a>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo8" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Mirror Effect</a>
+                        <div class="collapse" id="demo8">
+                          <a href="showmirror" class="list-group-item">Show Me</a>
+                          <a href="savemirror" class="list-group-item">Save with Mirror</a>
+                        </div>
+                      </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo6" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Rotation Effect</a>
+                        <div class="collapse" id="demo6">
+                          <a href="showrotate" class="list-group-item">Show Me</a>
+                          <a href="saverotate" class="list-group-item">Save with Rotation</a>
+                        </div>
+                      </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo10" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Lightblue Effect</a>
+                        <div class="collapse" id="demo10">
+                          <a href="showblue" class="list-group-item">Show Me</a>
+                          <a href="saveblue" class="list-group-item">Save with Lightblue</a>
+                        </div>
+                      </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div id="MainMenu">
+                      <div class="list-group panel">
+                        <a href="#demo9" class="list-group-item list-group-item-success" data-toggle="collapse" data-parent="#MainMenu">Lightred Effect</a>
+                        <div class="collapse" id="demo9">
+                          <a href="showred" class="list-group-item">Show Me</a>
+                          <a href="savered" class="list-group-item">Save with Lightred</a>
+                        </div>
+                      </div>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -83,11 +180,17 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1>Your Image</h1>
-                        <p><a href="#menu-toggle" class="btn btn-primary" id="menu-toggle">Toggle Menu</a></p>
+                        <div class="twobuttons">
+                            <p>
+                                <a href="#menu-toggle" class="btn btn-primary" id="menu-toggle">Toggle Menu</a>
+                                <a href="download" class="btn btn-success" id="menu-toggle">Download Your Image</a>
+                            </p>
+                        </div>
+
 
                         <?php
                         $img = Image::make(public_path('uploads/theimage.jpg'));
-                        $img->resize(800, null, function ($constraint) {
+                        $img->resize(600, null, function ($constraint) {
                         $constraint->aspectRatio();
                         });
                         $img->encode('png');
